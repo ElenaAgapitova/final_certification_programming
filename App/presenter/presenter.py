17f1d65a59c14bb5cb382ccdf11afaedf956294c
Elena Agapitova <agapitovae87@gmail.com>
2023-07-25 10:13:06 +0300

    Сhanged diagram

--- App/presenter/presenter.py
+++ App/presenter/presenter.py
@@ -1,16 +1,17 @@
 from data.dbsqlite_manager import SqLiteManager
+from view.abstract_view import View
 
 
 class Presenter:
 
-    def __init__(self, view, log_registry, path: str):
+    def __init__(self, view: View, log_registry, data: SqLiteManager):
         self.view = view
         self.log_registry = log_registry
         self.view.set_presenter(self)
-        self.__data = SqLiteManager(path)
+        self.data = data
 
     def read_db(self):
-        return self.__data.read_db(self.log_registry)
+        return self.data.read_db(self.log_registry)
 
     def get_tabl_registry(self):
         return self.log_registry.tabl_registry
@@ -25,7 +26,7 @@ class Presenter:
         self.log_registry.add_animal(kind, name, command, birth_date)
 
     def save_animal_into_bd(self, type_id, kind, name, command, birth_date):
-        return self.__data.save_animal(type_id, kind, name, command, birth_date)
+        return self.data.save_animal(type_id, kind, name, command, birth_date)
 
     def find_animal(self, index):
         return self.log_registry.find_animal(index)
@@ -40,4 +41,4 @@ class Presenter:
         return self.log_registry.number_of_animals()
 
     def save_command(self, index):
-        return self.__data.save_command(index, self.log_registry)
+        return self.data.save_command(index, self.log_registry)
--- App/service.py
+++ App/service.py
@@ -1,3 +1,4 @@
+from data.dbsqlite_manager import SqLiteManager
 from model.registry_animals import RegistryAnimals
 from presenter.presenter import Presenter
 from view.console import Console
@@ -7,7 +8,8 @@ class Service:
     def __init__(self):
         self.registry = RegistryAnimals()
         self.view = Console()
-        Presenter(self.view, self.registry, 'human_friends2.db')
+        self.data = SqLiteManager('human_friends2.db')
+        Presenter(self.view, self.registry, self.data)
 
     def start(self):
         self.view.start()
